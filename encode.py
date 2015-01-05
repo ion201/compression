2fbbfb74dcc4c86ebee7ea6a9aaaa5f21f168d20
nate <moniseat101@yahoo.com>
2015-01-05 08:49:09 -0700

    init files

---
+++ encode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: ./Encode [file] [quality 0-9]')
+        return
+
+
+if __name__ == '__main__':
+    main()
