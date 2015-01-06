559c7c00fd0dd8e010e4ab0eb3d85f24c8c7089b
nate <moniseat101@yahoo.com>
2015-01-06 16:58:03 -0700

    Processing pixels in c now works. Reduced time over 90%

--- Encode.py
+++ Encode.py
@@ -5,6 +5,8 @@ import struct
 import sys
 import math
 
+import ProcessPixels as PP
+
 
 def stripalpha(img):
     pixel_array = img.load()
@@ -50,8 +52,8 @@ def encode(in_file, quality=4):
     encoded_img = Image.new('RGB', orig_img.size)
 
     # 16-256 colors (4-8 bits correspond to quality 0-4) Each color is encoded by 6 bytes (RRGGBB).
-    color_map = {}
-    scan_quality = quality
+    # color_map = {}
+    # scan_quality = quality
     # while len(color_map) < 2**(quality+4)-1 and scan_quality < 5:
     #     color_map = {}
     #     pix_array_orig = orig_img.load()
@@ -69,30 +71,34 @@ def encode(in_file, quality=4):
     #     if scan_quality == 5:  # past 5, the quality gets worse
     #         scan_quality = 4.3  # Magic numbers are fun! It's close to, but not quite, 100**(1/3)
 
-    while len(color_map) < 2**(quality+4)-1 and scan_quality < 5:
-        color_map = {}
-        band_data = []
-        for i in range(3):
-            band_data.append(list(orig_img.getdata(i)))
-        for i in range(0, len(band_data[0]), 1):
-            pixel = tuple(min(band_data[band][i] // (100-math.floor(scan_quality**3))
-                          * (100-math.floor(scan_quality**3))
-                          + (100-math.floor(scan_quality**3)) // 3, 255)
-                          for band in range(3))
-            try:
-                color_map[pixel] += 1
-            except KeyError:
-                color_map[pixel] = 1
-        scan_quality += 1
-        if scan_quality == 5:
-            scan_quality = 4.3
+    # while len(color_map) < 2**(quality+4)-1 and scan_quality < 5:
+    #     color_map = {}
+    #     band_data = []
+    #     for i in range(3):
+    #         band_data.append(list(orig_img.getdata(i)))
+    #     for i in range(0, len(band_data[0]), 1):
+    #         pixel = tuple(min(band_data[band][i] // (100-math.floor(scan_quality**3))
+    #                           * (100 - math.floor(scan_quality ** 3))
+    #                           + (100-math.floor(scan_quality**3)) // 3, 255)
+    #                       for band in range(3))
+    #         try:
+    #             color_map[pixel] += 1
+    #         except KeyError:
+    #             color_map[pixel] = 1
+    #     scan_quality += 1
+    #     if scan_quality == 5:
+    #         scan_quality = 4.3
 
-    # The above two methods are about the same time to execute
-    # TODO: Build a c module to replace this python code.
+    band_data = []
+    for i in range(3):
+        band_data.append(list(orig_img.getdata(i)))
+    color_map = PP.organize(quality, band_data)
 
     # Format: ((r, g, b), count)
     color_priority = sorted(color_map.items(), key=lambda item: item[1],
                             reverse=True)[:2**(quality+4)-2]  # -2 because the max value (eg 1111) is treated seperately
+    print(len(color_priority))
+    print(color_priority[:10])
 
     b_header = _genheader(encoded_img)
     b_palette = _genpalette(color_priority)
--- c_modules/ProcessPixels.c
+++ c_modules/ProcessPixels.c
@@ -1,30 +1,77 @@
 #include <Python.h>
 
+int binColor(PyObject *c, float quality)
+{
+    // PyObject *c is a python long
+    int color = (int) PyLong_AsLong(c);
+    int qThreshold = 100 - (int)(quality * quality * quality);
+    int newColor = (color / qThreshold) * qThreshold + qThreshold / 3;
+    if (newColor > 255)
+        newColor = 255;
+    return newColor;
+}
+
 static PyObject *ProcessPixels_organize(PyObject *self, PyObject *args)
 {
-    // https://docs.python.org/3/c-api/arg.html
-    //Args: organize(int quality, PythonList pixels)
+    // https://docs.python.org/3/c-api/concrete.html#sequence-objects
+
+    //Args: organize(int quality, PyListObject pixels)
     //What this file needs to do:
     //1. Accept a list of pixels in the format: [(r1, r2, ...), (g1, g2, ...), (b1, b2, ...)]
-    //2. Iterate through the list. For every RGB pixel, so this python op:
+    //2. Iterate through the list. For every number, do this operation (in c of course)
     //   pixel = tuple(min(pixel_data[band][i] // (100-math.floor(scan_quality**3))
     //                 * (100-math.floor(scan_quality**3))
     //                 + (100-math.floor(scan_quality**3)) // 3, 255)
     //                 for band in range(3))
     //3. Return a dictionary for every calculated pixel and it's count
+    //   This requires buidling the Python Dict while iterating
+    //   or
+    //3. Return a list of the top x pixels. Discard counts. (This sounds easier!)
+    //   Don't build the PyObject until iteration is done
 
     int quality;
-    PyObject *pixelData;
+    PyObject *pixelData;    //PyListObject
+    PyObject *band_r;       //PyTupleObject
+    PyObject *band_g;
+    PyObject *band_b;
+    PyObject *pixelsDict;   //PyDictObject
+    PyObject *pixelTmp;     //PyTubpleObject
+    int i, countTmp;
+    int r, g, b;
 
-    if (!PyArg_ParseTuple(args, "iO", &quality, pixelData))
+    if (!PyArg_ParseTuple(args, "iO!", &quality, &PyList_Type, &pixelData))
         return NULL;
 
-    return Py_BuildValue("i", 3);
+    // Other list functions: PyList_GetItem(listObj, i)
+    band_r = PyList_GetItem(pixelData, 0);
+    band_g = PyList_GetItem(pixelData, 1);
+    band_b = PyList_GetItem(pixelData, 2);
+
+    pixelsDict = PyDict_New();
+    //pixelTmp = PyTuple_New(3);
+    for (i = 0; i < PyList_Size(band_r); i++){
+        r = binColor(PyList_GetItem(band_r, i), quality);
+        g = binColor(PyList_GetItem(band_g, i), quality);
+        b = binColor(PyList_GetItem(band_b, i), quality);
+        pixelTmp = PyTuple_New(3);
+        PyTuple_SetItem(pixelTmp, 0, Py_BuildValue("i", r));
+        PyTuple_SetItem(pixelTmp, 1, Py_BuildValue("i", g));
+        PyTuple_SetItem(pixelTmp, 2, Py_BuildValue("i", b));
+
+        // if (PyDict_Contains(pixelsDict, pixelTmp) == 1)
+        if (PyDict_GetItem(pixelsDict, pixelTmp) == NULL){
+            countTmp = 1;
+        } else{
+            countTmp = PyLong_AsLong(PyDict_GetItem(pixelsDict, pixelTmp)) + 1;
+        }
+        PyDict_SetItem(pixelsDict, pixelTmp, Py_BuildValue("i", countTmp));
+    }
+    return pixelsDict;
 }
 
 static PyMethodDef ProcessPixelsMethods[] = {
     {"organize", ProcessPixels_organize, METH_VARARGS,
-    "Accepts a list of pixels [(R, R...), (G, G...), (B, B...)] and returns a dict with the count for each"},
+    "organize(quality, pixels)\nAccepts a list of pixels [[R, R...], [G, G...], [B, B...]] and returns a dict with the count for each"},
     {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef ProcessPixelsModule = {
