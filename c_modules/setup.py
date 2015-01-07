abc60845281b2c1e7e19871adcfaf02185fbd0bf
ion201 <moniseat101@yahoo.com>
2015-01-06 21:47:21 -0700

    Linux update

--- c_modules/setup.py
+++ c_modules/setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup, Extension
 import shutil
 import os
+import platform
 
 setup(
     ext_modules=[Extension("ProcessPixels", ["ProcessPixels.c"])]
 )
 
 try:
-    shutil.copy('build/lib.win32-3.3/ProcessPixels.pyd', '../')
+    if platform.system() == 'Linux':
+        os.system('cp build/lib.linux-x86_64-3.*/ProcessPixels.cpython-*.so ../')
+    else:
+        shutil.copy('build/lib.win32-3.*/ProcessPixels.pyd', '../')
 except PermissionError:
     print('Error copying the new .pyd file. Is the old file open in a python terminal?')
